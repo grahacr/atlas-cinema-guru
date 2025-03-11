e154251c836f26c47fc210e96aaac034d6c34665
grahacr <courtney.graham@atlasschool.com>
2025-03-11 15:04:21 -0500

    further auth set up with github

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { signIn } from "@/auth";
+
 export default async function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-      Hello Cinema Guru
+      <form
+      action={async () => {
+        "use server";
+        await signIn("default", {redirectTo: "/ui" });
+      }}
+      >
+        <button className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+          <div>Sign In</div>
+          </button>
+      </form>
     </div>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,4 +1,8 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { fetchUser } from "./lib/data";
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -6,7 +10,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [
+    GitHub({}),
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      //@ts-ignore
+    authorize: async (credentials: { email: string; password: string }) => {
+      const { email, password } = credentials;
+      const user = await fetchUser(email);
+      if (!user) return null; //@ts-ignore
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+      if (passwordsMatch) return user;
+      return null;
+    },
+    })
+  ],
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
