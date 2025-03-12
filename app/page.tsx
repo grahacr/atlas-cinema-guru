65e6db376291d8c80a91aee44911d925d3199136
grahacr <courtney.graham@atlasschool.com>
2025-03-11 23:08:01 -0500

    revert some of the excess auth i tried

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,7 @@
-
-import { signIn } from "@/auth";;
-
-export default function Page() {
+export default async function Page() {
   return (
-    <main className="w-screen py-12 md:py-24 lg:py-32 flex flex-col items-center justify-center">
-      <div className="container px-4 md:px-6">
-        <div className="flex lg:flex-row flex-col gap-4 items-center">
-          <div className="space-y-4">
-          <form
-              action={async () => {
-                "use server";
-                await signIn("default", { redirectTo: "/" });
-              }}>
-                <button className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
-                  <div>Sign In</div>
-                </button>
-              </form>
-              </div>
-              </div>
-        </div>
-    </main>
+    <div className="flex items-center justify-center h-screen">
+      Hello Cinema Guru
+    </div>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github"
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -7,17 +7,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [
-    GitHubProvider({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET
-    }),
-  ],
+  providers: [GitHub],
   callbacks: {
-    async redirect({ url, baseUrl }) {
-      return baseUrl + "/";
-    },
-    async authorized({ auth }) {
+    authorized: async ({ auth }) => {
       return !!auth;
     }
   },
